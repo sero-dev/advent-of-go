95933e28bb33627bf49ff2b9b3fb61a3530ece59
Sean Rodriguez <sean.rodriguez@outlook.com>
2024-08-12 22:43:36 -0400

    Add solution for Day 02 Problem 02 and unit tests

--- 2015/day02/day02.go
+++ 2015/day02/day02.go
@@ -23,7 +23,7 @@ func Problem1(bufferReader *bufio.Reader) (int, error) {
 			return 0, err
 		}
 
-		dimensionPattern := regexp.MustCompile(`^(\d+)x(\d+)x(\d+)\n$`)
+		dimensionPattern := regexp.MustCompile(`(\d+)x(\d+)x(\d+)`)
 
 		matches := dimensionPattern.FindStringSubmatch(line)
 
@@ -41,6 +41,40 @@ func Problem1(bufferReader *bufio.Reader) (int, error) {
 	}
 
 	return totalArea, nil
+}
+
+func Problem2(bufferReader *bufio.Reader) (int, error) {
+	totalRibbonNeeded := 0
+
+	for {
+		line, err := bufferReader.ReadString('\n')
+
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			return 0, err
+		}
+
+		dimensionPattern := regexp.MustCompile(`(\d+)x(\d+)x(\d+)`)
+
+		matches := dimensionPattern.FindStringSubmatch(line)
+
+		if (len(matches) != 4) {
+			return 0, errors.New("string " + line + " was not formatted correctly")
+		}
+
+		length, _ := strconv.Atoi(matches[1])
+		width, _ := strconv.Atoi(matches[2])
+		height, _ := strconv.Atoi(matches[3])
+
+		smallestSide := min(2 * (length + width), 2 * (width + height), 2 * (height + length))
+
+		totalRibbonNeeded += length * width * height + smallestSide
+	}
+
+	return totalRibbonNeeded, nil
 }
 
 func getSurfaceArea(length int, width int, height int) int {
--- 2015/day02/day02_test.go
+++ 2015/day02/day02_test.go
@@ -29,3 +29,27 @@ func TestProblem1WhenInputIs1x1x10(t *testing.T) {
 		t.Fatalf("Problem1() with %s input returned %d, instead of %d, with %v error", inputString, actual, expect, err)
 	}
 }
+
+func TestProblem2WhenInputIs2x3x4(t *testing.T) {
+	inputString := "2x3x4\n"
+	input := util.CreateBufferReaderFrom(inputString)
+
+	actual, err := Problem2(input)
+	expect := 34
+
+	if actual != expect || err != nil {
+		t.Fatalf("Problem2() with %s input returned %d, instead of %d, with %v error", inputString, actual, expect, err)
+	}
+}
+
+func TestProblem2WhenInputIs1x1x10(t *testing.T) {
+	inputString := "1x1x10\n"
+	input := util.CreateBufferReaderFrom(inputString)
+
+	actual, err := Problem2(input)
+	expect := 14
+
+	if actual != expect || err != nil {
+		t.Fatalf("Problem2() with %s input returned %d, instead of %d, with %v error", inputString, actual, expect, err)
+	}
+}
